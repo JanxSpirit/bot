7fde562ec883ebe0152778c2885f739e1486f22d
JanxSpirit <greggcarrier@gmail.com>
2016-06-25 16:49:55 -0400

    bot

--- bot.clj
+++ bot.clj
@@ -1,8 +1,7 @@
 (defn run
   [{:keys [arena state bot_id energy spawn-bot?] :as step-details}]
-  (println "arena - " (count arena))
-  {:commands [{:cmd "SHOOT"
-               :metadata {:direction (rand-nth [0 1 2 3 4])
-                          :energy 1}}
+  (println "arena -- " (count arena))
+  {:commands [{:cmd "MOVE"
+               :metadata {:direction (rand-nth [0 1 2 3 4])}}
               {:cmd "SET_STATE"
                :metadata {:foo "bar"}}]})
