b39363a6a9ddd20cfaa145a65d6ff69360741f06
JanxSpirit <greggcarrier@gmail.com>
2016-07-24 11:56:33 -0400

    test

--- bot.clj
+++ bot.clj
@@ -1,6 +1,6 @@
 (defn run
-  [{:keys  [arena saved-state bot_id energy spawn-bot? initiative-order wombat-count] :as frame-details}]
-  (println bot_id ": my initiative order is " initiative-order " of " wombat-count " wombats")
+  [{:keys  [arena saved-state bot-id energy spawn-bot? initiative-order wombat-count] :as frame-details}]
+  (println bot-id ": my initiative order is " initiative-order " of " wombat-count " wombats")
   (let [command-options [[{:cmd  "SHOOT"
                            :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}
                           {:cmd  "SET_STATE"
