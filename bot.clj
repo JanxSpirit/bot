9a8c1e2c15654aa937298dcb808021bcb66606b6
JanxSpirit <greggcarrier@gmail.com>
2016-07-24 16:52:04 -0400

    smoke

--- bot.clj
+++ bot.clj
@@ -1,11 +1,11 @@
 (defn run
   [{:keys  [arena saved-state bot-id energy spawn-bot? initiative-order wombat-count] :as frame-details}]
   (println bot-id ": my initiative order is " initiative-order " of " wombat-count " wombats")
-  (let [command-options [[{:cmd  "SHOOT"
+  (let [command-options [#_[{:cmd  "SHOOT"
                            :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}
                           {:cmd  "SET_STATE"
                            :metadata  {:foo  "bar"}}]
-                         [{:cmd  "MOVE"
+                         #_[{:cmd  "MOVE"
                            :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]
-                         {:cmd "SMOKESCREEN"}]]
+                         [{:cmd "SMOKESCREEN"}]]]
     {:commands (rand-nth command-options)}))
