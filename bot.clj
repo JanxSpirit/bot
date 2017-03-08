e7a0db34289fb42ada79ea0578c2a981c254e6c4
Gregg Carrier <greggcarrier@gmail.com>
2017-03-07 23:53:19 -0500

    tweaks

--- bot.clj
+++ bot.clj
@@ -2,10 +2,14 @@
   (def turn-directions [:right :left :about-face])
   (def smoke-directions [:forward :backward :left :right :drop])
 
-  (defn immediate-squares [dungeon] {:up (get (get dungeon 2) 3)
-                                     :left (get (get dungeon 3) 2)
-                                     :right (get (get dungeon 3) 4)
-                                     :down (get (get dungeon 4) 3)})
+  (defn immediate-squares [dungeon] {:north (get (get dungeon 2) 3)
+                                     :west (get (get dungeon 3) 2)
+                                     :east (get (get dungeon 3) 4)
+                                     :south (get (get dungeon 4) 3)})
+
+  (defn my-orientation [arena] (get-in (find #(= :wombat (get-in % [:contents :type])) (flatten arena)) [:contents :orientation]))
+
+  (defn food-in-front-of-me [immed-sqs ])
 
   (let [command-options [(repeat 10 {:action :move
                                      :metadata {}})
@@ -14,8 +18,10 @@
                          (repeat 4 {:action :shoot
                                       :metadata {}})
                          (repeat 1 {:action :smoke
-                                    :metadata {:direction (rand-nth smoke-directions)}})]]
+                                    :metadata {:direction (rand-nth smoke-directions)}})]
+        immed-sqs (immediate-squares (state :arena))]
+    (if )
 
-    (let [cleanstate (dissoc state :saved-state :up-square)]
+    (let [cleanstate (dissoc state :saved-state :up-square :my-orientation)]
       {:command (rand-nth (flatten command-options))
-       :state (assoc cleanstate :immediate-squares (immediate-squares (cleanstate :arena)))})))
+       :state (assoc cleanstate :immediate-squares (immediate-squares (cleanstate :arena)) :my-orientation (my-orientation (cleanstate :arena)))})))
