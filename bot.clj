fce4cf45c88d697b060041b660c8ba4c4f076e1c
Gregg Carrier <greggcarrier@gmail.com>
2017-03-07 22:40:31 -0500

    get up square

--- bot.clj
+++ bot.clj
@@ -2,6 +2,8 @@
   (def turn-directions [:right :left :about-face])
   (def smoke-directions [:forward :backward :left :right :drop])
 
+  (defn up-square [dungeon] (get (get map 2) 3))
+
   (let [command-options [(repeat 10 {:action :move
                                      :metadata {}})
                          (repeat 2 {:action :turn
@@ -12,4 +14,4 @@
                                     :metadata {:direction (rand-nth smoke-directions)}})]]
 
     {:command (rand-nth (flatten command-options))
-     :state {}}))
+     :state (assoc state :up-square (up-square (:arena state)))}))
