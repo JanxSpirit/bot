a74cf2e8799e1844c4df31ec83c1f4852439f76f
Gregg Carrier <greggcarrier@gmail.com>
2017-03-09 20:28:30 -0500

    remove global coords for now

--- bot.clj
+++ bot.clj
@@ -175,7 +175,8 @@
                    (merge explored {(get-in cell [:contents :uuid]) true})
                    (add-to-sorted-arena sorted-arena
                                         cell
-                                        (update frontier-node
+                                        frontier-node
+                                        #_(update frontier-node
                                                 :coords
                                                 update-global-coords-fn))))))))
 
